f15fbf9fbd449fbc3aecc9c3e6335bcf195ef1e7
Bharat Mutkure <mutkurebharat@gmail.com>
2018-03-05 11:55:06 +0530

    Unneessary Code has been Removed

--- calc.js
+++ calc.js
@@ -12,18 +12,15 @@
                                }
                 function p(val){
                                   lastOperation = false;
-
                                   document.getElementById("display").value += val;
                                   if(val == '+' || val == '-' || val == '*' || val == '/'){
                                   operator = val;
                                   var index = document.getElementById("display").value.indexOf(operator);
                                   console.log(operator);
                                   }
-                                  if(val == '1' || val == '2' || val == '3' || val == '4' || val == '5' || val == '6' || val == '7'  || val == '8'  || val == '9' || val == '0'){
                                   number = document.getElementById("display").value.substring(document.getElementById("display").value.lastIndexOf(operator));
                                   console.log(number)
                                   }
-                              }
                   function e(){
                                   z = document.getElementById("display").value;
                                   if(z.includes('+')||z.includes('-')||z.includes('*')||z.includes('/')){
